ffba22ee0498a1ab17c3925380ea68a6680cac91
FiereEinar <nickxylanmelloria@gmail.com>
2024-03-28 14:59:00 +0800

    feature: date and time

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ struct Item
     char name[20];
     int stocks;
     double price;
+    char dateAdded[30];
+    char lastUpdated[30];
 };
 
 // structure of an individual node in a linked list
@@ -27,6 +29,7 @@ int getListSize(const struct Node *head);
 void updateStock(struct Node **head);
 void addTestItem(struct Node **head, char name[], int stocks, double price);
 char* getCurrentTime();
+char* getCurrentDate();
 void printHelp();
 
 int main()
@@ -37,6 +40,7 @@ int main()
     char x[15];
     printf("\nType 'help' to see all available commands.");
 
+    printf("\nAdding test items for developments purposes only.");
     addTestItem(&head, "pencil", 25, 10);
     addTestItem(&head, "paper", 50, 40);
     addTestItem(&head, "scissors", 15, 56);
@@ -56,50 +60,53 @@ int main()
     return 0;
 }
 
-void updateStock(struct Node **head)
+void addItem(struct Node **head)
 {
     system("cls");
+    printf("Adding an item.\n\n");
+    // create a new instance of the Item struct
+    struct Item newItem;
+
+    // prompt the user with all the data and store it in that newItem structure
+    printf("Enter the name of item: ");
+    scanf("%s", newItem.name);
+
+    printf("Enter the stocks of item: ");
+    scanf("%d", &newItem.stocks);
+
+    printf("Enter the price of item: ");
+    scanf("%lf", &newItem.price);
+
+    sprintf(newItem.dateAdded, "%s | %s", getCurrentDate(), getCurrentTime());
+    sprintf(newItem.lastUpdated, "%s | %s", getCurrentDate(), getCurrentTime());
+    // we then create a new Node structure, it is dynamically allocated in the memory
+    // so that it doesn't get cleared when this function exits.
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    // here we just set the data property of the Node to our Item struct
+    newNode->data = newItem;
+    // the next will be null since this new Item Node will be added at the end of the list
+    newNode->next = NULL;
+
+    // if this is the first item that is added then we simply set the head to our newly created Node
+    if (*head == NULL) {
+        *head = newNode;
+    // otherwise we need to traverse the end of the list and add it there
+    } else {
+        // here we create a (current) variable to prevent our head from being manipulated
+        struct Node *current = *head;
+        // it is guaranteed that at the end of the list, its next property will always be NULL
+        // so we use it to check wether of not we have reached the end of the list
+        while(current->next != NULL)
+        {
+            // we traverse the list by simply setting our current pointer to point to the next Node
+            // this will continue until the next property in NULL and that means we have reach the end
+            current = current->next;
+        }
+        // after we reached the end, we simply set the next pointer to point to our newly created Node
+        current->next = newNode;
+    }
     printLinkedlist(*head);
-
-    char toUpdate[10];
-    int index;
-
-    printf("\n\nEnter the index of the item: ");
-    scanf("%d", &index);
-
-    printf("\nWhat do you want to update?[name/stock/price]: ");
-    scanf("%s", toUpdate);
-
-    struct Node *current = *head;
-    int j = 1;
-
-    while(index != j)
-    {
-        current = current->next;
-        j++;
-    }
-
-    if (strcmp(toUpdate, "name") == 0)
-    {
-        printf("Enter a new name: ");
-        scanf("%s", current->data.name);
-    }
-    else if (strcmp(toUpdate, "stock") == 0)
-    {
-        printf("Enter an updated stock: ");
-        scanf("%d", &current->data.stocks);
-    }
-    else if (strcmp(toUpdate, "price") == 0)
-    {
-        printf("Enter an updated price: ");
-        scanf("%lf", &current->data.price);
-    }
-    else {
-        printf("\nError: Check your spelling");
-    }
-
-    printf("\nUpdated Successfully!");
-    printLinkedlist(*head);
+    printf("\nItem added!");
 }
 
 void deleteItem(struct Node **head)
@@ -147,8 +154,55 @@ void deleteItem(struct Node **head)
     // free the memory
     free(deleted);
     // after deleting the item, reprint the list
-    printf("\nItem deleted successfully!");
     printLinkedlist(*head);
+    printf("\nItem deleted successfully!");
+}
+
+void updateStock(struct Node **head)
+{
+    system("cls");
+    printLinkedlist(*head);
+
+    int toUpdate, index;
+
+    printf("\n\nEnter the index of the item: ");
+    scanf("%d", &index);
+
+    printf("\nOptions: [ 1. name | 2. stocks | 3. price ]");
+    printf("\nWhat do you want to update?: ");
+    scanf("%d", &toUpdate);
+
+    // we create a current variable so that we don't manipulate the pointer of the head
+    struct Node *current = *head;
+    int j = 1;
+    // traverse to the Node/item that the user wants to update
+    while(index != j)
+    {
+        current = current->next;
+        j++;
+    }
+
+    // after that, we simply determine what the user wants to update and prompt the user for new data
+    switch(toUpdate) {
+    case 1:
+        printf("\nEnter a new name: ");
+        scanf("%s", current->data.name);
+        break;
+    case 2:
+        printf("\nEnter an updated stock: ");
+        scanf("%d", &current->data.stocks);
+        break;
+    case 3:
+        printf("\nEnter an updated price: ");
+        scanf("%lf", &current->data.price);
+        break;
+    default:
+        printf("\nError: Incorrect number entered.");
+        break;
+    }
+
+    printLinkedlist(*head);
+    printf("\nUpdated Successfully!");
 }
 
 int getListSize(const struct Node *head)
@@ -175,65 +229,53 @@ void printHelp()
     printf("\n6. update \t update an item to inventory.");
 }
 
-char* getCurrentTime()
-{
+char* getCurrentTime() {
     time_t now;
     struct tm *localTime;
-    static char timeString[9]; // Hour:Minute:Seconds
+    static char timeString[12]; // Hour:Minute AM/PM
 
     time(&now);
     localTime = localtime(&now);
 
-    sprintf(timeString, "%02d:%02d:%02d", localTime->tm_hour, localTime->tm_min, localTime->tm_sec);
+    int hour = localTime->tm_hour;
+    int minute = localTime->tm_min;
+    char period[3]; // AM or PM
+
+    if (hour >= 12) {
+        strcpy(period, "PM");
+        if (hour > 12) {
+            hour -= 12;
+        }
+    } else {
+        strcpy(period, "AM");
+        if (hour == 0) {
+            hour = 12;
+        }
+    }
+
+    // Format the time string
+    sprintf(timeString, "%02d:%02d %s", hour, minute, period);
 
     return timeString;
 }
 
-void addItem(struct Node **head)
+char* getCurrentDate()
 {
-    system("cls");
-    printf("Adding an item.\n\n");
-    // create a new instance of the Item struct
-    struct Item newItem;
+    time_t currentTime;
+    struct tm *localTime;
+    static char dateString[11];
 
-    // prompt the user with all the data and store it in that newItem structure
-    printf("Enter the name of item: ");
-    scanf("%s", newItem.name);
+    time(&currentTime);
+    localTime = localtime(&currentTime);
 
-    printf("Enter the stocks of item: ");
-    scanf("%d", &newItem.stocks);
+    int year = localTime->tm_year + 1900;
+    int month = localTime->tm_mon + 1;
+    int day = localTime->tm_mday;
 
-    printf("Enter the price of item: ");
-    scanf("%lf", &newItem.price);
+    // Format the date string
+    sprintf(dateString, "%d-%02d-%02d", year, month, day);
 
-    // we then create a new Node structure, it is dynamically allocated in the memory
-    // so that it doesn't get cleared when this function exits.
-    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-    // here we just set the data property of the Node to our Item struct
-    newNode->data = newItem;
-    // the next will be null since this new Item Node will be added at the end of the list
-    newNode->next = NULL;
-
-    // if this is the first item that is added then we simply set the head to our newly created Node
-    if (*head == NULL) {
-        *head = newNode;
-    // otherwise we need to traverse the end of the list and add it there
-    } else {
-        // here we create a (current) variable to prevent our head from being manipulated
-        struct Node *current = *head;
-        // it is guaranteed that at the end of the list, its next property will always be NULL
-        // so we use it to check wether of not we have reached the end of the list
-        while(current->next != NULL)
-        {
-            // we traverse the list by simply setting our current pointer to point to the next Node
-            // this will continue until the next property in NULL and that means we have reach the end
-            current = current->next;
-        }
-        // after we reached the end, we simply set the next pointer to point to our newly created Node
-        current->next = newNode;
-    }
-    printf("\nItem added!");
-    printLinkedlist(*head);
+    return dateString;
 }
 
 // for development purposes only
@@ -244,6 +286,9 @@ void addTestItem(struct Node **head, char name[], int stocks, double price)
     strcpy(newItem.name, name);
     newItem.stocks = stocks;
     newItem.price = price;
+
+    sprintf(newItem.dateAdded, "%s | %s", getCurrentDate(), getCurrentTime());
+    sprintf(newItem.lastUpdated, "%s | %s", getCurrentDate(), getCurrentTime());
 
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     newNode->data = newItem;
@@ -267,12 +312,13 @@ void printLinkedlist(const struct Node *p)
 {
     system("cls");
     int i = 1;
-    printf("\nItem:\t\t\tStocks:\t\tPrice:");
+    printf("\nItem:\t\t\tStocks:\t\tPrice:\t\tDate Added:\t\t\tLast Updated:");
     // here we are using the same logic that we've used in the addItem function,
     // but instead we are printing each the contents.
     while (p != NULL)
     {
-        printf("\n%d. %-20s\t%d\t\tP%.2lf", i, p->data.name, p->data.stocks, p->data.price);
+        printf("\n%d. %-20s\t%d\t\tP%.2lf\t\t%s\t\t%s",
+               i, p->data.name, p->data.stocks, p->data.price, p->data.dateAdded, p->data.lastUpdated);
         p = p->next;
         i++;
     }
